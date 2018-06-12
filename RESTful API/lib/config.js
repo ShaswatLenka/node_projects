8a8fc8a21a2f6b8d2e187ef7ed544e6504f50a7f
shaswat <shaswatlenka10@outlook.com>
2018-06-12 14:04:02 +0530

    add checks

--- RESTful API/lib/config.js
+++ RESTful API/lib/config.js
@@ -1,33 +1,34 @@
 /*
-Create and export configuration variables
-*/
+ * Create and export configuration variables
+ *
+ */
 
-
-//container for all the environments
+// Container for all environments
 var environments = {};
 
-//staging object (default)
+// Staging (default) environment
 environments.staging = {
-'httpPort' : 3000,
-'httpsPort': 3001,
-'envName' : 'staging',
-'hashingSecret':'thisIsASecret'
-
+  'httpPort' : 3000,
+  'httpsPort' : 3001,
+  'envName' : 'staging',
+  'hashingSecret' : 'thisIsASecret',
+  'maxChecks' : 5
 };
-//production object
+
+// Production environment
 environments.production = {
-'httpPort' : 5000,
-'httpsPort': 5001,
-'envName': 'production',
-'hashingSecret': 'thisIsAlsoASecret'
-
+  'httpPort' : 5000,
+  'httpsPort' : 5001,
+  'envName' : 'production',
+  'hashingSecret' : 'thisIsAlsoASecret',
+  'maxChecks' : 10
 };
 
-//determine which environment was passed as a command line argument
+// Determine which environment was passed as a command-line argument
 var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
 
-//check that the entered environment equals to the currentEnvironment else default to staging
+// Check that the current environment is one of the environments above, if not default to staging
 var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
 
-//export the module
+// Export the module
 module.exports = environmentToExport;
--- RESTful API/lib/data.js
+++ RESTful API/lib/data.js
@@ -1,95 +1,106 @@
 /*
-*
-*library for storing and editing data
-*/
+ * Library for storing and editing data
+ *
+ */
 
-//Dependencies
-const fs = require('fs');
-const path = require('path');
+// Dependencies
+var fs = require('fs');
+var path = require('path');
+var helpers = require('./helpers');
 
-//container for the module to be exported
-var lib={};
+// Container for module (to be exported)
+var lib = {};
 
-//Base directory of the data folder
-lib.baseDir = path.join(__dirname, '/../.data/');
+// Base directory of data folder
+lib.baseDir = path.join(__dirname,'/../.data/');
 
-//Write data to a file
-lib.create = function(dir, file, data, callback){
-  //open the file for writing
-  fs.open(lib.baseDir+dir+'/'+file+'.json','wx',function(err, fileDescriptor){
-    if (!err && fileDescriptor){
-      //convert data to string
+// Write data to a file
+lib.create = function(dir,file,data,callback){
+  // Open the file for writing
+  fs.open(lib.baseDir+dir+'/'+file+'.json', 'wx', function(err, fileDescriptor){
+    if(!err && fileDescriptor){
+      // Convert data to string
       var stringData = JSON.stringify(data);
-      //write to file and close it
-      fs.writeFile(fileDescriptor, stringData, function(err){
+
+      // Write to file and close it
+      fs.writeFile(fileDescriptor, stringData,function(err){
         if(!err){
-          fs.close(fileDescriptor, function(err){
+          fs.close(fileDescriptor,function(err){
             if(!err){
               callback(false);
-            }else{
-              callback('error while closing the file');
+            } else {
+              callback('Error closing new file');
             }
           });
-        }else{
-          callback('error writing to file');
+        } else {
+          callback('Error writing to new file');
         }
       });
-    }else{
-      callback('could not open new file, it may already exist');
+    } else {
+      callback('Could not create new file, it may already exist');
     }
-
   });
+
 };
 
-//read data from file
+// Read data from a file
 lib.read = function(dir,file,callback){
-  fs.readFile(lib.baseDir+dir+'/'+file+'.json', 'utf-8', function(err, data){
-    callback(err, data);
+  fs.readFile(lib.baseDir+dir+'/'+file+'.json', 'utf8', function(err,data){
+    if(!err && data){
+      var parsedData = helpers.parseJsonToObject(data);
+      callback(false,parsedData);
+    } else {
+      callback(err,data);
+    }
   });
 };
 
-//update data inside a file
-lib.update = function(dir, file, data, callback){
-  fs.open(lib.baseDir+dir+'/'+file+'.json','r+', function(err, fileDescriptor){
-    if(!err){
+// Update data in a file
+lib.update = function(dir,file,data,callback){
+
+  // Open the file for writing
+  fs.open(lib.baseDir+dir+'/'+file+'.json', 'r+', function(err, fileDescriptor){
+    if(!err && fileDescriptor){
+      // Convert data to string
       var stringData = JSON.stringify(data);
-      fs.truncate(fileDescriptor, function(err){
+
+      // Truncate the file
+      fs.truncate(fileDescriptor,function(err){
         if(!err){
-          fs.writeFile(fileDescriptor,stringData,function(err){
+          // Write to file and close it
+          fs.writeFile(fileDescriptor, stringData,function(err){
             if(!err){
-              fs.close(fileDescriptor, function(err){
+              fs.close(fileDescriptor,function(err){
                 if(!err){
-                  callback('noerror');
-                }else{
-                  callback('error closing file');
+                  callback(false);
+                } else {
+                  callback('Error closing existing file');
                 }
               });
-            }else{
-              callback('error writing to file');
+            } else {
+              callback('Error writing to existing file');
             }
           });
-        }else{
-          callback('error truncating file');
+        } else {
+          callback('Error truncating file');
         }
       });
-    }else{
-      callback('the file does not exist');
+    } else {
+      callback('Could not open file for updating, it may not exist yet');
     }
   });
+
 };
 
-//delete a file
+// Delete a file
 lib.delete = function(dir,file,callback){
-  //unlink the file from filesystem
-  fs.unlink(lib.baseDir+dir+'/'+file+'.json',function(err){
-    if(!err){
-      callback('file deleted successfully');
-    }else{
-      callback('error deleting the file');
-    }
+
+  // Unlink the file from the filesystem
+  fs.unlink(lib.baseDir+dir+'/'+file+'.json', function(err){
+    callback(err);
   });
+
 };
 
-
-//export the module
+// Export the module
 module.exports = lib;
